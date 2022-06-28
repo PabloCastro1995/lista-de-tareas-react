6d3168822d75956fde82df62716b9b28fa6bba58
Pablo Castro <pabloc_95@hotmail.com>
2022-06-28 10:24:47 -0300

    probando commit

--- lista-tareas/src/Componentes/Mostrar/Mostrar.jsx
+++ lista-tareas/src/Componentes/Mostrar/Mostrar.jsx
@@ -5,13 +5,25 @@ export default function Mostrar({ listaDeTareas }) {
   return (
     <>
       <h3>Tareas</h3>
-      {listaDeTareas ? (
-        listaDeTareas.map((t) => <span>{t.descripcion}</span>)
-      ) : (
+      {listaDeTareas.lenght === 0 ? (
         <p id="mensaje-lista-vacia">Parece que no hay nada por aquí!</p>
-      )}
+        
+      ) : (
+ 
+        <ul id="lista-tareas">
+        {listaDeTareas.map((t) => 
+        <li
+          key={t.id}
+          className={t.prioridad}>
+          {t.descripcion}
+        </li>)
+          
+          }
+        </ul>
 
-      <ul id="lista-tareas"></ul>
+      )}
+        
+      
 
       <p class="tips">Tip: Pueden borrar tareas clickeando en ellas</p>
     </>
